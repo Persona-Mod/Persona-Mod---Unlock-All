8560d10b4debe1ed5305388fb5d075cccb39074c
Cpt-Hazama <Caleb123459@gmail.com>
2022-06-20 02:08:29 -0400

    a

---
+++ lua/autorun/persona_dlc_unlockeverything.lua
@@ -0,0 +1,10 @@
+/*--------------------------------------------------
+	*** Copyright (c) 2012-2021 by Cpt. Hazama, All rights reserved. ***
+	No parts of this code or any of its contents may be reproduced, copied, modified or adapted,
+	without the prior written consent of the author, unless otherwise indicated for stand-alone materials.
+--------------------------------------------------*/
+if (!file.Exists("autorun/persona_main.lua","LUA")) then return end
+
+// Hehe, you've located the holy grail of addons >=)
+
+PERSONA_UNLOCKALL = true
